f58ac86da329e0318cd436442440a8a7d616f187
Andres0607MP <andresmancera06156@gmail.com>
2026-08-22 10:00:36 -0500

    Nevo test

--- sample_app.py
+++ sample_app.py
@@ -8,7 +8,7 @@ sample = Flask(__name__)
 @sample.route ("/")
 def home():
 	try:
-		conn = pymysql.connect(host='servidor-bd-082', user='root', password='sena123', database='082_db')
+		conn = pymysql.connect(host='servidor-bd-erroneo', user='root', password='sena123', database='082_db')
 		conn.close()
 		db_status = 'Conexion exitosa a la base de datos, prueba de CI/CD para despliegue continuo'
 	except Exception as e:
--- test_app.py
+++ test_app.py
@@ -1,2 +1,18 @@
-def test_ejemplo():
-    assert 1 + 1 == 2
+import pytest
+from sample_app import sample
+
+
+@pytest.fixture
+def client():
+    sample.config["TESTING"] = True
+
+    with sample.test_client() as client:
+        yield client
+
+
+def test_pagina_principal(client):
+    respuesta = client.get("/")
+
+    assert respuesta.status_code == 200
+    assert b"Bienvenido a mi aplicacion Flask" in respuesta.data
+
